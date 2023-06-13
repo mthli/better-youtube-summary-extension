9410e1970a5d2793c563a495fe1f2f66778c4ef0
Matthew Lee <matthewlee0725@gmail.com>
2023-06-13 23:38:24 +0800

    feat: observe page url changed

--- src/contentScript.tsx
+++ src/contentScript.tsx
@@ -1,11 +1,18 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 
 const App = () => {
+  const [pageUrl, setPageUrl] = useState(location.href)
+
+  useEffect(() => {
+    const observer = new MutationObserver(() => setPageUrl(location.href))
+    observer.observe(document, { subtree: true, childList: true })
+    return () => observer.disconnect()
+  }, [])
 
   useEffect(() => {
     // TODO
-  }, [])
+  }, [pageUrl])
 
   return (
     <div />
