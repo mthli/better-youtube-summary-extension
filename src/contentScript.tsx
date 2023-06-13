9964f3dc4490e678f4920c53fa48052889cd2dc5
Matthew Lee <matthewlee0725@gmail.com>
2023-06-13 22:24:58 +0800

    chore: create react app

--- src/contentScript.tsx
+++ src/contentScript.tsx
@@ -1,9 +1,12 @@
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-  if (msg.color) {
-    console.log("Receive color = " + msg.color);
-    document.body.style.backgroundColor = msg.color;
-    sendResponse("Change color to " + msg.color);
-  } else {
-    sendResponse("Color message is none.");
-  }
-});
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return (
+    <div />
+  )
+}
+
+const root = document.createElement('div')
+document.body.appendChild(root)
+ReactDOM.render(<App />, root)
