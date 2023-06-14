81065f1db06422d6d1bc4216cf92c0a2895a36b1
Matthew Lee <matthewlee0725@gmail.com>
2023-06-14 20:18:56 +0800

    build: import i18next

---
+++ src/i18n.ts
@@ -0,0 +1,26 @@
+import i18n from 'i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import { initReactI18next } from 'react-i18next'
+
+const resources = {
+  en: {
+    translation: {
+      'chat': 'Chat ...',
+      'processing': 'Processing ...',
+    },
+  },
+}
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'en',
+
+    interpolation: {
+      escapeValue: false,
+    },
+  })
+
+export default i18n
