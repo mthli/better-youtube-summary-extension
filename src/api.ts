8daa223303f03f79bc21a169d69c004575671ce6
Matthew Lee <matthewlee0725@gmail.com>
2023-06-15 15:51:43 +0800

    chore: clean code

--- src/api.ts
+++ src/api.ts
@@ -7,46 +7,46 @@ const TAG = 'api'
 const BASE_URL = 'https://bys.mthli.com'
 
 export const parseVid = (pageUrl: string): string => {
-    // log(TAG, `parseVid, pageUrl=${pageUrl}`)
+  // log(TAG, `parseVid, pageUrl=${pageUrl}`)
 
-    // https://github.com/fczbkk/UrlMatch
-    const pageUrlMatch = new UrlMatch([
-        'https://*.youtube.com/watch*?v=*',
-    ])
+  // https://github.com/fczbkk/UrlMatch
+  const pageUrlMatch = new UrlMatch([
+    'https://*.youtube.com/watch*?v=*',
+  ])
 
-    const match = pageUrlMatch.test(pageUrl)
-    if (!match) return ''
+  const match = pageUrlMatch.test(pageUrl)
+  if (!match) return ''
 
-    const url = new URL(pageUrl)
-    const params = new URLSearchParams(url.search)
-    const vid = params.get('v') ?? ''
-    if (!vid) return ''
+  const url = new URL(pageUrl)
+  const params = new URLSearchParams(url.search)
+  const vid = params.get('v') ?? ''
+  if (!vid) return ''
 
-    return vid
+  return vid
 }
 
 export const summarize = async (
-    vid: string,
-    chapters?: Chapter[],
-    noTranscript?: boolean,
+  vid: string,
+  chapters?: Chapter[],
+  noTranscript?: boolean,
 ): Promise<Response> => {
-    // log(TAG, `summarize, vid=${vid}, chapters=${JSON.stringify(chapters)}`)
+  // log(TAG, `summarize, vid=${vid}, chapters=${JSON.stringify(chapters)}`)
 
-    const res = await fetch(`${BASE_URL}/api/summarize/${vid}`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            'chapters': chapters ?? [],
-            'no_transcript': Boolean(noTranscript),
-        }),
-    })
+  const res = await fetch(`${BASE_URL}/api/summarize/${vid}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      'chapters': chapters ?? [],
+      'no_transcript': Boolean(noTranscript),
+    }),
+  })
 
-    if (!res.ok) {
-        const msg = await res.json()
-        throw new Error(msg)
-    }
+  if (!res.ok) {
+    const msg = await res.json()
+    throw new Error(msg)
+  }
 
-    return res
+  return res
 }
