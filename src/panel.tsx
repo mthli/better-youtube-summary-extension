236ff5d0ed1168dc9523ea126bd7d2b518890e81
Matthew Lee <matthewlee0725@gmail.com>
2023-06-20 04:40:32 +0800

    chore: clean code

--- src/panel.tsx
+++ src/panel.tsx
@@ -322,21 +322,20 @@ const Panel = ({ pageUrl }: { pageUrl: string }) => {
                     >
                       {
                         // SVG copied from YouTube Settings, not perfect but ok.
-                        !translating && (
-                          <svg
-                            viewBox='0 0 24 24'
-                            width='22px'
-                            height='22px'
-                            style={{
-                              display: 'flex',
-                              alignItems: 'center',
-                              padding: '1px',
-                              fill: iconColor,
-                            }}
-                          >
-                            <path d="M13.33 6c-1 2.42-2.22 4.65-3.57 6.52l2.98 2.94-.7.71-2.88-2.84c-.53.67-1.06 1.28-1.61 1.83l-3.19 3.19-.71-.71 3.19-3.19c.55-.55 1.08-1.16 1.6-1.83l-.16-.15c-1.11-1.09-1.97-2.44-2.49-3.9l.94-.34c.47 1.32 1.25 2.54 2.25 3.53l.05.05c1.2-1.68 2.29-3.66 3.2-5.81H2V5h6V3h1v2h7v1h-2.67zM22 21h-1l-1.49-4h-5.02L13 21h-1l4-11h2l4 11zm-2.86-5-1.86-5h-.56l-1.86 5h4.28z" />
-                          </svg>
-                        )
+                        !translating &&
+                        <svg
+                          viewBox='0 0 24 24'
+                          width='22px'
+                          height='22px'
+                          style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            padding: '1px',
+                            fill: iconColor,
+                          }}
+                        >
+                          <path d="M13.33 6c-1 2.42-2.22 4.65-3.57 6.52l2.98 2.94-.7.71-2.88-2.84c-.53.67-1.06 1.28-1.61 1.83l-3.19 3.19-.71-.71 3.19-3.19c.55-.55 1.08-1.16 1.6-1.83l-.16-.15c-1.11-1.09-1.97-2.44-2.49-3.9l.94-.34c.47 1.32 1.25 2.54 2.25 3.53l.05.05c1.2-1.68 2.29-3.66 3.2-5.81H2V5h6V3h1v2h7v1h-2.67zM22 21h-1l-1.49-4h-5.02L13 21h-1l4-11h2l4 11zm-2.86-5-1.86-5h-.56l-1.86 5h4.28z" />
+                        </svg>
                       }
                       {
                         translating &&
